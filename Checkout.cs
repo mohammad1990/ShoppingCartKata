36a48bfa38c2205f46d0f1f8355cdab66c104012
mohammad tofi <>
2019-08-03 14:25:56 +0200

    complete calculatesTotalPrice

--- Checkout.cs
+++ Checkout.cs
@@ -27,6 +27,63 @@ namespace Shopping
                 {"banana",new Offer{Discount=130,NumberOfPiece=3}}
             };
         }
+        private int total_price;
+        public int Total_price { get; set; }
+        public int calculatesTotalPrice()
+        {
+
+            foreach (KeyValuePair<string, int> entry in _itemCount)
+            {
+                scanItem(entry.Key, entry.Value);
+            }
+            return Total_price;
+        }
+        public void scanItem(string key, int value)
+        {
+            if (OfferFound(key))
+            {
+                int numberOfCoverDiscount = GetOfferNumber(key);
+                numberOfCoverDiscount = value - numberOfCoverDiscount;
+                if (numberOfCoverDiscount == 0)
+                {
+                    int discountPrice = GetOfferPrice(key);
+                    Total_price += discountPrice;
+                }
+                else if (numberOfCoverDiscount > 0)
+                {
+                    int discountPrice = GetOfferPrice(key);
+                    Total_price += discountPrice;
+                    Total_price += calculatePrice(key, numberOfCoverDiscount);
+                }
+                else if (numberOfCoverDiscount < 0)
+                {
+                    Total_price += calculatePrice(key, value);
+                }
+            }
+            else
+            {
+                Total_price += calculatePrice(key, value);
+            }
+        }
+        private int calculatePrice(string key, int value)
+        {
+            int itemPrice = 0;
+            itemPrice = scan(key);
+            return value * itemPrice;
+        }
+        private int scan(string item)
+        {
+            if (_prices.ContainsKey(item))
+            {
+                int itemPrices = 0;
+                if (_prices.TryGetValue(item, out itemPrices))
+                {
+                    return itemPrices;
+                }
+            }
+            return -1;
+
+        }
         private bool OfferFound(string item)
         {
             if (_offers.ContainsKey(item))
